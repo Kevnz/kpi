be30fc9c8a8c2ca930922c06caa36bd639226c81
Kevin Isom <kevin.isom@gmail.com>
2019-05-05 18:18:23 +1200

    initial broadcast work

---
+++ src/server/routes/messages.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    method: 'GET',
+    path: '/send',
+    config: {
+      handler: (r, h) => {
+        r.server.broadcast('welcome!')
+        return 'SENT'
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/h',
+    config: {
+      id: 'hello',
+      handler: (request, h) => {
+        return 'world!'
+      },
+    },
+  },
+]
