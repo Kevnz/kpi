ffce873da5ef9dc6ef535683d4bae51274740016
Kevin Isom <kevin.isom@gmail.com>
2019-05-06 07:44:15 +1200

    Post model

---
+++ src/server/models/post.js
@@ -0,0 +1,52 @@
+const faker = require('faker')
+
+const createPost = () => {
+  const postTitle = faker.company.catchPhrase()
+  return {
+    id: faker.random.uuid(),
+    slug: faker.internet.slug(),
+    title: postTitle,
+    category: faker.commerce.department(),
+    description: `${faker.commerce.productAdjective()} ${faker.commerce.productMaterial()}. ${faker.company.catchPhrase()}.`,
+    image: faker.image.imageUrl(),
+    body: faker.lorem.paragraphs(faker.random.number({ min: 3, max: 10 })),
+  }
+}
+
+module.exports = {
+  get: createPost,
+  getAll: () => {
+    return [
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+      createPost(),
+    ]
+  },
+}
