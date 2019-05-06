0444c1f40e350f319521446b396ca6ff92762485
Kevin Isom <kevin.isom@gmail.com>
2019-05-06 13:34:03 +1200

    use cache and match resolvers for queries

--- src/server/graphql/resolvers.js
+++ src/server/graphql/resolvers.js
@@ -1,17 +1,70 @@
+const Cache = require('@brightleaf/cache')
 const { User, Post, Product } = require('../models')
 const { getToken } = require('../utils/auth')
+
+const redisConfig = {
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_HOST,
+}
+
+if (process.env.REDIS_PASSWORD) {
+  redisConfig.password = process.env.REDIS_PASSWORD
+}
+
+const cache = new Cache({ prepend: 'kpi', redis: redisConfig })
+
+const POSTS_KEY = 'all-blog-posts'
+
+const PRODUCTS_KEY = 'all-products'
+
 const resolvers = {
   Query: {
     user: async (root, args, context, info) => {
-      return User.getAll()
+      return User.get()
+    },
+    users: async (root, args, context, info) => {
+      const users = await cache.get('all-users')
+      if (users) {
+        return users
+      }
+      const genUsers = User.getAll()
+      cache.set('all-users', genUsers)
+      return genUsers
+    },
+    product: async (root, args, context, info) => {
+      const products = await cache.get(PRODUCTS_KEY)
+      if (products) {
+        const one = products.find(p => p.id === args.id)
+        if (one && one.length > 0) return one[0]
+        return products[0]
+      }
+      return Product.get()
     },
     products: async (root, args, context, info) => {
-      return Product.getAll()
+      const products = await cache.get(PRODUCTS_KEY)
+      if (products) {
+        return products
+      }
+      const genProds = Product.getAll()
+      cache.set(PRODUCTS_KEY, genProds)
+      return genProds
     },
     posts: async (root, args, context, info) => {
-      return Post.getAll()
+      const posts = await cache.get(POSTS_KEY)
+      if (posts) {
+        return posts
+      }
+      const genPosts = Post.getAll()
+      cache.set(POSTS_KEY, genPosts)
+      return genPosts
     },
     post: async (root, args, context, info) => {
+      const posts = await cache.get(POSTS_KEY)
+      if (posts) {
+        const one = posts.find(p => p.slug === args.slug)
+        if (one && one.length > 0) return one[0]
+        return posts[0]
+      }
       return Post.get()
     },
   },
