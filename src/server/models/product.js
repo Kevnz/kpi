b977287868537a29d7c9cb823f45367759d7d423
Kevin Isom <kevin.isom@gmail.com>
2019-05-06 13:51:34 +1200

    picture

--- src/server/models/product.js
+++ src/server/models/product.js
@@ -1,13 +1,41 @@
 const faker = require('faker')
+/*
+
+abstract animals business cats city food nightlife fashion people nature sports technics transport
+*/
+const picArr = [
+  'abstract',
+  'animals',
+  'business',
+  'cats',
+  'city',
+  'food',
+  'nightlife',
+  'fashion',
+  'people',
+  'nature',
+  'sports',
+  'technics',
+  'transport',
+]
+const getPicture = category => {
+  if (picArr.contains(category.toLoweCase())) {
+    return `http://lorempixel.com/320/240/${category}`
+  }
+  return `http://lorempixel.com/320/240/${
+    picArr[faker.random.number({ min: 0, max: picArr.length - 1 })]
+  }`
+}
 
 const createProduct = () => {
+  const category = faker.commerce.department()
   return {
     id: faker.random.uuid(),
     name: faker.commerce.productName(),
     price: faker.commerce.price(),
-    category: faker.commerce.department(),
+    category,
     description: `${faker.commerce.productAdjective()} ${faker.commerce.productMaterial()}. ${faker.company.catchPhrase()}.`,
-    image: faker.image.imageUrl(),
+    image: getPicture(category),
   }
 }
 
