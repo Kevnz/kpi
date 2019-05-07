fa1157e7650701ec533842a48922c0682c6cd9ab
Kevin Isom <kevin.isom@gmail.com>
2019-05-08 11:33:53 +1200

    update to expose multiple methods

--- src/server/utils/ga.js
+++ src/server/utils/ga.js
@@ -1,6 +1,11 @@
 const ua = require('universal-analytics')
 const CODE = process.env.GOOGLE_ANALYTICS
 const visitor = ua(CODE)
-module.exports = (cat, action) => {
-  visitor.event(cat, action).send()
+module.exports = {
+  page: (route, title) => {
+    visitor.pageview(route, 'https://kev-pi.herokuapp.com/', title).send()
+  },
+  event: (cat, action) => {
+    visitor.event(cat, action).send()
+  },
 }
