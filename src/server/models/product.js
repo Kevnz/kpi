6d9aac0c1951f7afaae10fa0965c45966602f594
Kevin Isom <kevin.isom@gmail.com>
2019-05-16 13:11:04 +1200

    https

--- src/server/models/product.js
+++ src/server/models/product.js
@@ -20,11 +20,11 @@ const picArr = [
 ]
 const getPicture = category => {
   if (picArr.includes(category.toLowerCase())) {
-    return `http://lorempixel.com/320/240/${category}?slide=${faker.random.number(
+    return `https://lorempixel.com/320/240/${category}?slide=${faker.random.number(
       { min: 0, max: 1000 }
     )}`
   }
-  return `http://lorempixel.com/320/240/${
+  return `https://lorempixel.com/320/240/${
     picArr[faker.random.number({ min: 0, max: picArr.length - 1 })]
   }?slide=${faker.random.number({ min: 0, max: 1000 })}`
 }
