31c1a3fee0b5a4ed1572de0cf745328e45504687
Kevin Isom <kevin.isom@gmail.com>
2019-12-11 11:12:24 +1300

    remove and use fetch

--- src/server/utils/package.js
+++ src/server/utils/package.js
@@ -1,6 +1,6 @@
-const npmstat = require('npmstat')
+// const npmstat = require('npmstat')
 const { mapper, delay } = require('@kev_nz/async-tools')
-
+const fetch = require('node-fetch')
 /* const days = require('days-in-a-row');
 const allTheDays = days(new Date('10/16/2019'), 2);
 
@@ -9,10 +9,28 @@ const mappedRanges = allTheDays.map((d, i) => {
   return `${d}:${allTheDays[i+1]}`
 })
 mappedRanges.pop()
+
+(name, { range = 'last-month', type } = {}) => {
+  const api = 'https://api.npmjs.org/downloads'
+
+  if (type === 'range') {
+    return axios(`${api}/range/${range}/${name}`)
+      .then(({ data }) => data)
+      .catch(({ response: { data: { error } } }) => error)
+  }
+
+  return axios(`${api}/point/${range}/${name}`)
+    .then(({ data }) => data)
+    .catch(({ response: { data: { error } } }) => error)
+}
+
+
 */
 
 module.exports = async (pkgName, range) => {
-  const down = await npmstat.getDownloads(pkgName, { range })
-  console.log('the download', down)
-  return down
+  const api = 'https://api.npmjs.org/downloads'
+  const down = await fetch(`${api}/range/${range}/${pkgName}`)
+  const data = await down.json()
+  console.log('the download', data)
+  return data
 }
